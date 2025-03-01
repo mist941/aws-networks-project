663667708fd39d75961eb2207ae5b5610b06a868
Ivan <statkevich.ivan999@gmail.com>
2025-03-01 17:31:30 +0200

    Add security groups for bastion host, private EC2, and ALB in VPC module

--- modules/aws-vpc/main.tf
+++ modules/aws-vpc/main.tf
@@ -84,30 +84,139 @@ resource "aws_nat_gateway" "public_nat_gateway" {
   }
 }
 
-data "aws_ssm_parameter" "client_vpn_certificate" {
-  name = "vpn_server_certificate_arn"
-}
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion_sg"
+  description = "Cloudflare Tunnel or VPN"
+  vpc_id      = aws_vpc.main_vpc.id
 
-resource "aws_cloudwatch_log_group" "client_vpn_log_group" {
-  name              = "client_vpn_log_group"
-  retention_in_days = 30
-}
-
-resource "client_vpn_endpoint" "vpn" {
-  description            = "Client VPN endpoint"
-  server_certificate_arn = data.aws_ssm_parameter.client_vpn_certificate.value
-  client_cidr_block      = "10.0.0.0/22"
-  connection_log_options {
-    enabled              = true
-    cloudwatch_log_group = aws_cloudwatch_log_group.client_vpn_log_group.name
+  ingress {
+    description = "Allow SSH from internal VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
   }
-  authentication_options {
-    type                       = "certificate-authentication"
-    root_certificate_chain_arn = data.aws_ssm_parameter.client_vpn_certificate.value
+
+  ingress {
+    description = "Allow HTTPS from anywhere (Cloudflare Tunnel/VPN)"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description     = "Allow outbound SSH to private EC2"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.private_ec2_sg.id]
+  }
+
+  egress {
+    description = "Allow outbound HTTPS for updates"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_client_vpn_network_association" "vpn_assoc" {
-  client_vpn_endpoint_id = client_vpn_endpoint.vpn.id
-  subnet_id              = aws_subnet.private_subnets[0].id
+resource "aws_security_group" "private_ec2_sg" {
+  name        = "private_ec2_sg"
+  description = "Private EC2 security group"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description     = "Allow SSH from Bastion Host"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  ingress {
+    description = "Allow HTTP from ALB"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.public_subnet_ciders
+  }
+
+  ingress {
+    description = "Allow HTTPS from ALB"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.public_subnet_ciders
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb_sg"
+  description = "Security Group for Application Load Balancer"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description = "Allow HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description     = "Allow all outbound traffic to private EC2 instances"
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "-1"
+    security_groups = [aws_security_group.private_ec2_sg.id]
+  }
+
+  depends_on = [aws_security_group.private_ec2_sg]
+}
+
+
+
+# data "aws_ssm_parameter" "client_vpn_certificate" {
+#   name = "vpn_server_certificate_arn"
+# }
+
+# resource "aws_cloudwatch_log_group" "client_vpn_log_group" {
+#   name              = "client_vpn_log_group"
+#   retention_in_days = 30
+# }
+
+# resource "client_vpn_endpoint" "vpn" {
+#   description            = "Client VPN endpoint"
+#   server_certificate_arn = data.aws_ssm_parameter.client_vpn_certificate.value
+#   client_cidr_block      = "10.0.0.0/22"
+#   connection_log_options {
+#     enabled              = true
+#     cloudwatch_log_group = aws_cloudwatch_log_group.client_vpn_log_group.name
+#   }
+#   authentication_options {
+#     type                       = "certificate-authentication"
+#     root_certificate_chain_arn = data.aws_ssm_parameter.client_vpn_certificate.value
+#   }
+# }
+
+# resource "aws_client_vpn_network_association" "vpn_assoc" {
+#   client_vpn_endpoint_id = client_vpn_endpoint.vpn.id
+#   subnet_id              = aws_subnet.private_subnets[0].id
+# }
