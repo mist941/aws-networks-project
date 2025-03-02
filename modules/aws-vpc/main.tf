a990586a1f2c0beb3d46ab584ed237a4cc9c2e72
Ivan <statkevich.ivan999@gmail.com>
2025-03-02 09:21:11 +0200

    Remove commented-out VPN configuration from VPC module

--- modules/aws-vpc/main.tf
+++ modules/aws-vpc/main.tf
@@ -298,34 +298,3 @@ resource "aws_instance" "bastion_host" {
     Name = "bastion_host"
   }
 }
-
-# Commented out VPN configuration
-# Can be uncommented and configured when VPN access is needed
-# Requires valid certificates stored in SSM Parameter Store
-# data "aws_ssm_parameter" "client_vpn_certificate" {
-#   name = "vpn_server_certificate_arn"
-# }
-
-# resource "aws_cloudwatch_log_group" "client_vpn_log_group" {
-#   name              = "client_vpn_log_group"
-#   retention_in_days = 30
-# }
-
-# resource "client_vpn_endpoint" "vpn" {
-#   description            = "Client VPN endpoint"
-#   server_certificate_arn = data.aws_ssm_parameter.client_vpn_certificate.value
-#   client_cidr_block      = "10.0.0.0/22"
-#   connection_log_options {
-#     enabled              = true
-#     cloudwatch_log_group = aws_cloudwatch_log_group.client_vpn_log_group.name
-#   }
-#   authentication_options {
-#     type                       = "certificate-authentication"
-#     root_certificate_chain_arn = data.aws_ssm_parameter.client_vpn_certificate.value
-#   }
-# }
-
-# resource "aws_client_vpn_network_association" "vpn_assoc" {
-#   client_vpn_endpoint_id = client_vpn_endpoint.vpn.id
-#   subnet_id              = aws_subnet.private_subnets[0].id
-# }
